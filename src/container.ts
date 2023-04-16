96a9b5859b4c09c956f74b1aa7b6e2a359796752
sebastiaanvanspauwen <sebastiaan.vanspauwen@appwise.be>
2023-04-16 21:16:36 +0200

    Added RED monitoring for prom client.

--- src/container.ts
+++ src/container.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import http, { ServerResponse } from 'http'
 import express, { Express } from 'express'
+import { Histogram, Counter } from 'prom-client'
 import { register } from './metrics'
 
 export abstract class AppContainer {
@@ -8,6 +9,13 @@ export abstract class AppContainer {
 
   private state: 'starting' | 'ready' | 'shutdown' | 'unknown'
   private server?: http.Server
+
+  private readonly ERROR_RANGE_START = 500
+  private readonly ERROR_RANGE_END = 600
+
+  private readonly httpRequestDuration: Histogram<string>
+  private readonly httpRequestCounter: Counter<string>
+  private readonly httpErrorCounter: Counter<string>
 
   constructor () {
     console.log('starting server')
@@ -18,6 +26,31 @@ export abstract class AppContainer {
     process.on('SIGINT', () => { void this.destroy() })
     process.on('SIGUSR2', () => { void this.destroy() })
     process.on('SIGHUP', () => { void this.destroy() })
+
+    // Rate
+    this.httpRequestCounter = new Counter({
+      name: 'http_request_total',
+      help: 'Total number of HTTP requests made.',
+      labelNames: ['method', 'route', 'status'],
+      registers: [register]
+    })
+
+    // Error
+    this.httpErrorCounter = new Counter({
+      name: 'http_error_total',
+      help: 'Total number of HTTP errors.',
+      labelNames: ['method', 'route', 'status'],
+      registers: [register]
+    })
+
+    // Duration
+    this.httpRequestDuration = new Histogram({
+      name: 'http_request_duration_seconds',
+      help: 'Duration of HTTP requests in seconds.',
+      labelNames: ['method', 'route', 'status'],
+      registers: [register],
+      buckets: [0.1, 0.2, 0.5, 1, 2, 5, 10]
+    })
 
     void this.up().then(() => { void this.initialize() })
   }
@@ -30,6 +63,32 @@ export abstract class AppContainer {
     this.app.get('/', (_, res) => this.version(res))
     this.app.get('/health', (_, res) => this.liveness(res))
     this.app.get('/ready', (_, res) => this.readiness(res))
+
+    this.app.use((req, res, next) => {
+      const end = this.httpRequestDuration.startTimer()
+      const route = req.path ?? 'unknown_route'
+
+      res.on('finish', () => {
+        const { statusCode } = res
+
+        const isError = statusCode >= this.ERROR_RANGE_START && statusCode < this.ERROR_RANGE_END
+
+        const labels = {
+          method: req.method,
+          route,
+          status: statusCode
+        }
+
+        this.httpRequestCounter.inc(labels)
+
+        if (isError) this.httpErrorCounter.inc(labels)
+
+        end(labels)
+      })
+
+      next()
+    })
+
     this.app.get('/metrics', (_, res) => this.metrics(res))
 
     this.populate(this.app)
